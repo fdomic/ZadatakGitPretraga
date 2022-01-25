5aaa47f0a57c520dc8e70796f229aa8a52735be3
Filip Domic <filipdomic@gmail.com>
2022-01-25 13:29:17 +0100

    3

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -7,16 +7,25 @@ use Illuminate\Support\Facades\Http;
 
 class ResultController extends Controller
 {
-    public function getResult( Request $term){
-        $RocksResponse = Http::get('https://api.github.com/search/issues?q=php%20rocks');
-        $SucksResponse = Http::get('https://api.github.com/search/issues?q=php%20sucks'); 
+    public function getResult( Request $request){
+
+        $RocksResponse = Http::get('https://api.github.com/search/issues?q='.$request->has('term').'%20rocks');
+        $SucksResponse = Http::get('https://api.github.com/search/issues?q='.$request->has('term').'%20sucks'); 
+        
+        $rock = $RocksResponse->json("total_count");
+        $sucks = $SucksResponse->json("total_count");
+        $score = 10 * $rock / ($rock + $sucks );
         
 
-        $rock =$RocksResponse->json("total_count");
-        $suck =$SucksResponse->json("total_count");
+        $response = array(
+
+            "rock" => $rock,
+            "suck" => $sucks,
+            "score" => $score ,
+
+        );
         
-        
-        return response()->json($suck, 200);
+        return response()->json($response, 200);
 
     }
 
